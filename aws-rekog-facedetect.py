4759e5917baaec277ac85958ffedebe46483b48b
jit2600@gmail.com <jit2600@gmail.com>
2019-04-25 18:38:21 +0100

    aws rekog initial commit

---
+++ aws-rekog-facedetect.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+
+if __name__ == "__main__":
+    # Change bucket to your S3 bucket that contains the image file.
+    # Change photo to your image file.
+    photo='moviestar.jpg'
+    bucket='jit2600'
+    client=boto3.client('rekognition')
+
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
+
+    print('Detected faces for ' + photo)    
+    for faceDetail in response['FaceDetails']:
+        print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) 
+              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+        print('Here are the other attributes:')
+        print(json.dumps(faceDetail, indent=4, sort_keys=True))
