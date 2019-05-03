86f656c3ebed5f7ca112971979982ace93093003
jit2600@gmail.com <jit2600@gmail.com>
2019-05-03 18:53:05 +0100

    formatting change

--- aws-rekog-detect-label.py
+++ aws-rekog-detect-label.py
@@ -2,18 +2,12 @@ import boto3
 
 if __name__ == "__main__":
 
-    #imageFile='moviestar.jpg'
-    photo='moviestar.jpg'
     bucket='prasrekog'
+    photo='moviestar.jpg'
     client=boto3.client('rekognition')
 
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
     
-    #with open(imageFile, 'rb') as image:
-    #response = client.detect_labels(Image={'Bytes': image.read()})
-        
     print('Detected labels in ' + photo)    
     for label in response['Labels']:
         print (label['Name'] + ' : ' + str(label['Confidence']))
-
-    print('Done...')
