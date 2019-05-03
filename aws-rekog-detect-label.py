318a2df89bbf7a6da537605438a898ccfc9ba81a
jit2600@gmail.com <jit2600@gmail.com>
2019-05-03 18:50:05 +0100

    changed detect label function to read from S3 bucket

--- aws-rekog-detect-label.py
+++ aws-rekog-detect-label.py
@@ -2,13 +2,17 @@ import boto3
 
 if __name__ == "__main__":
 
-    imageFile='moviestar.jpg'
+    #imageFile='moviestar.jpg'
+    photo='moviestar.jpg'
+    bucket='prasrekog'
     client=boto3.client('rekognition')
-   
-    with open(imageFile, 'rb') as image:
-        response = client.detect_labels(Image={'Bytes': image.read()})
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+    
+    #with open(imageFile, 'rb') as image:
+    #response = client.detect_labels(Image={'Bytes': image.read()})
         
-    print('Detected labels in ' + imageFile)    
+    print('Detected labels in ' + photo)    
     for label in response['Labels']:
         print (label['Name'] + ' : ' + str(label['Confidence']))
 
